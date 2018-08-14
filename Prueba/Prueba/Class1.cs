fb8cc68526feafe91d1a57440a06b167ad538d68
Jonathan Castelán Pacheco <johncastelan20@gmail.com>
2018-08-14 16:49:43 -0500

    Realice una clase

---
+++ Prueba/Prueba/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba
+{
+    class Class1
+    {
+       public void Mensaje()
+        {
+            Console.WriteLine("Hola Mundo");
+        }
+    }
+}
